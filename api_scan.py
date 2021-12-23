4bac0e9b7a25266bee4328a5e0ca8a6ef9d05f55
mdaboo10 <58439831+Daboo-mohesh@users.noreply.github.com>
2021-12-23 14:41:17 +0400

    Update api_scan.py

--- api_scan.py
+++ api_scan.py
@@ -33,6 +33,11 @@ def data_uri_to_cv2_img(uri):
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     return img
 
+def readb64(uri):
+   encoded_data = uri.split(',')[1]
+   nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+   img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+   return img
 
 
 @app.route('/scanning', methods = ['GET','POST'])
